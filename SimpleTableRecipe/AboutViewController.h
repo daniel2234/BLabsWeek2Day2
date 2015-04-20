28f5f0e3fae911c00b5a74f980b0f81d8e3dfdf9
Daniel Kwiatkowski <kwiatkowski.daniel@gmail.com>
2015-04-20 15:29:36 -0400

    added About page with html and jpegs

---
+++ SimpleTableRecipe/AboutViewController.h
@@ -0,0 +1,15 @@
+//
+//  AboutViewController.h
+//  SimpleTableRecipe
+//
+//  Created by Daniel Kwiatkowski on 2015-04-20.
+//  Copyright (c) 2015 Daniel Kwiatkowski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController
+
+@property (strong,nonatomic) IBOutlet UIWebView *webView;
+
+@end
---
+++ SimpleTableRecipe/AboutViewController.m
@@ -0,0 +1,40 @@
+//
+//  AboutViewController.m
+//  SimpleTableRecipe
+//
+//  Created by Daniel Kwiatkowski on 2015-04-20.
+//  Copyright (c) 2015 Daniel Kwiatkowski. All rights reserved.
+//
+
+#import "AboutViewController.h"
+
+@interface AboutViewController ()
+
+@end
+
+@implementation AboutViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    NSURL *url = [NSURL fileURLWithPath:[[NSBundle mainBundle]pathForResource:@"about.html" ofType:nil]];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [self.webView loadRequest:request];
+}
+    
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
